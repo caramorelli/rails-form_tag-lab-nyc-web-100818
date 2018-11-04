37f95b94f246b4702133108bba4b7ef5e2fdb784
caramorelli7@gmail.com <caramorelli7@gmail.com>
2018-11-04 17:31:44 -0500

    create rails form-tag

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,26 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+  end
+
+  def new
   end
 
   def show
     @student = Student.find(params[:id])
   end
+
+  def create
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end 
+
 
 end
